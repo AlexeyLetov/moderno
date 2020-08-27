b22efbf6297ca60e034b3b1cf98c620fa0b23b39
AlexeyLetov <adec78@mail.ru>
2020-08-27 11:59:51 +0700

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
 $(function () {
 
-  var mixer = mixitup('.products__inner-box');
+  // var mixer = mixitup('.products__inner-box');
+
+  if ($('.products__inner-box').length) {
+    var mixer = mixitup('.products__inner-box');
+  }
 
   $(".rate-star").rateYo({
     rating: 5,
@@ -8,11 +12,31 @@ $(function () {
     readOnly: true
   });
 
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+  });
+
   $('.product-slider__inner').slick({
     dots: true,
     arrows: false,
     slidesToShow: 4,
     slidesToScroll: 4
+  });
+
+  $('.icon-th-list').on('click', function () {
+    $('.product__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+  });
+  $('.icon-th-large').on('click', function () {
+    $('.product__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
   });
 
 
